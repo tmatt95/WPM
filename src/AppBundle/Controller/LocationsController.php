ad9ac7fc317f08b235a22024d40a417dbdecf931
matthew <tmatt95@gmail.com>
2015-09-05 22:18:49 +0100

    Tidying up. This commit is working on loose ends at the end of the day.

--- src/AppBundle/Controller/LocationsController.php
+++ src/AppBundle/Controller/LocationsController.php
@@ -1,82 +1,97 @@
 <?php
 
-  // src/AppBundle/Controller/LocationsController.php
-  namespace AppBundle\Controller;
+// src/AppBundle/Controller/LocationsController.php
+
+namespace AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\JsonResponse;
-
 use AppBundle\Entity\Location;
 use AppBundle\Form\Locations\Location as FLocation;
 
-class LocationsController extends Controller
-{
+class LocationsController extends Controller {
 
-  public function addAction(Request $request)
-  {
+    public function addAction(Request $request) {
 
-    // Generates the form
-    $location = new Location();
-    $form = $this->createForm(new FLocation(),$location);
-    $form->handleRequest($request);
+        // Generates the form
+        $location = new Location();
+        $form = $this->createForm(new FLocation(), $location);
+        $form->handleRequest($request);
 
-    // If form is posted and valid, then saves
-    if ($form->isValid()) {
-      $em = $this->getDoctrine()->getManager();
-      $em->persist($location);
-      $em->flush();
+        // If form is posted and valid, then saves
+        if ($form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($location);
+            $em->flush();
+        }
+
+        $html = $this->container->get('templating')->render(
+                'locations/add.html.twig', array('form' => $form->createView())
+        );
+        return new Response($html);
     }
 
-    $html = $this->container->get('templating')->render(
-      'locations/add.html.twig',
-       array('form'=>$form->createView())
-    );
-    return new Response($html);
-  }
-
-  public function manageAction()
-  {
-    $html = $this->container->get('templating')->render(
-      'locations/manage.html.twig',
-      array()
-    );
-    return new Response($html);
-  }
-
-  public function getAction()
-  {
-    // Gets locations
-    $repository = $this->getDoctrine()
-    ->getRepository('AppBundle:Location');
-    $query = $repository->createQueryBuilder('l')
-    ->getQuery();
-    $locations = $query->getResult();
-
-    //Outputs to browser
-    $output = [];
-    foreach($locations as $l){
-      $output[] = [
-        'id'=>$l->getId(),
-        'name'=>$l->getName(),
-        'description' => $l->getDescription()
-      ];
+    public function manageAction() {
+        $html = $this->container->get('templating')->render(
+                'locations/manage.html.twig', array()
+        );
+        return new Response($html);
     }
-    $response = new JsonResponse();
-    $response->setData(
-      $output
-    );
-    return $response;
-  }
 
-  public function partsinAction()
-  {
-    $html = $this->container->get('templating')->render(
-      'locations/partsin.html.twig',
-      array()
-    );
-    return new Response($html);
-  }
+    public function getAction() {
+        // Gets locations
+        $repository = $this->getDoctrine()
+                ->getRepository('AppBundle:Location');
+        $query = $repository->createQueryBuilder('l')
+                ->getQuery();
+        $locations = $query->getResult();
+
+        //Outputs to browser
+        $output = [];
+        foreach ($locations as $l) {
+            $output[] = [
+                'id' => $l->getId(),
+                'name' => $l->getName(),
+                'description' => $l->getDescription()
+            ];
+        }
+        $response = new JsonResponse();
+        $response->setData(
+                $output
+        );
+        return $response;
+    }
+
+    public function partsinAction() {
+        $html = $this->container->get('templating')->render(
+                'locations/partsin.html.twig', array()
+        );
+        return new Response($html);
+    }
+
+    public function editAction($id, Request $request) {
+        
+        // Generates the form
+        $location = $this->getDoctrine()
+        ->getRepository('AppBundle:Location')
+        ->find($id);
+        $form = $this->createForm(new FLocation(), $location); 
+        
+        $form->handleRequest($request);
+
+        // If form is posted and valid, then saves
+        if ($form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($location);
+            $em->flush();
+        }
+        
+        $html = $this->container->get('templating')->render(
+                'locations/edit.html.twig', array('formLocation' => $form->createView())
+        );
+        return new Response($html);
+    }
+
 }
