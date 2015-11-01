e5255e86c627767aed6ad88fa5e61dceb4aa0d2d
matthew <tmatt95@gmail.com>
2015-11-01 19:54:19 +0000

    Final commit for thw weekend

--- src/AppBundle/Form/User/User.php
+++ src/AppBundle/Form/User/User.php
@@ -1,7 +1,18 @@
 <?php
 
-  // src/AppBundle/Form/User/User.php
-
+/**
+ * User Form
+ * The add user form.
+ * 
+ * PHP version 5.6
+ * 
+ * @category WPM
+ * @package  User
+ * @author   Matthew Turner <tmatt95@gmail.com>
+ * @license  http://opensource.org/licenses/GPL-3.0 GPL3
+ * @version  GIT: <1.0.0>
+ * @link     https://github.com/tmatt95/WPM/
+ */
 
 namespace AppBundle\Form\User;
 
@@ -9,8 +20,26 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
+/**
+ * User Form
+ * The add user form.
+ * 
+ * PHP version 5.6
+ * 
+ * @category WPM
+ * @package  User
+ * @author   Matthew Turner <tmatt95@gmail.com>
+ * @license  http://opensource.org/licenses/GPL-3.0 GPL3
+ * @version  Release: <1.0.0>
+ * @link     https://github.com/tmatt95/WPM/
+ */
 class User extends AbstractType
 {
+    /**
+     * Options for form
+     * @param OptionsResolver $resolver form options
+     * @return nothing
+     */
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults(
@@ -21,17 +50,57 @@ class User extends AbstractType
         );
     }
 
+    /**
+     * This is where the form elements are contained
+     * @param      FormBuilderInterface $builder form builder
+     * @param      array                $options for the form
+     * @return     nothing
+     */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('username', 'text', array('attr' => array('placeholder' => 'Username')))
-            ->add('name_first', 'text', array('label' => 'First Name', 'attr' => array('placeholder' => 'First Name')))
-            ->add('name_last', 'text', array('label' => 'Last Name', 'attr' => array('placeholder' => 'Last Name')))
-            ->add('email', 'email', array('attr' => array('placeholder' => 'Email')))
-            ->add('password', 'password', array('attr' => array('placeholder' => 'Password')))
+            ->add(
+                'username',
+                'text',
+                array(
+                    'attr' => array('placeholder' => 'Username')
+                )
+            )
+            ->add(
+                'name_first',
+                'text',
+                array(
+                    'label' => 'First Name',
+                    'attr' => array('placeholder' => 'First Name')
+                )
+            )
+            ->add(
+                'name_last',
+                'text',
+                array(
+                    'label' => 'Last Name',
+                    'attr' => array('placeholder' => 'Last Name')
+                )
+            )
+            ->add(
+                'email',
+                'email',
+                array('attr' => array('placeholder' => 'Email'))
+            )
+            ->add(
+                'password',
+                'password',
+                array('attr' => array('placeholder' => 'Password'))
+            )
             ->add('save', 'submit', array('label' => 'Add User'));
     }
 
+    /**
+     * Get the form name
+     *
+     * @inheritdoc
+     * @return     nothing
+     */
     public function getName()
     {
         return 'user';
